0ce5a8e68d2f0ef33072c96bd0828e503440541c
Flavio Aguilar <flavio.red0568@gmail.com>
2026-03-23 09:22:03 -0600

    here starts the test

--- Calculadora.java
+++ Calculadora.java
@@ -4,7 +4,7 @@ public class Calculadora
     public static void main (String [] args) 
     {
         Scanner teclado = new Scanner(System.in);
-       Operacion operacion = new Operacion();
+        int opcion, a, b;
 
         do { 
             System.out.println("0: Salir" +
@@ -13,12 +13,30 @@ public class Calculadora
                             "3: Multiplicar" +
                             "4: Dividir"
             );
-            operacion.setOpcion(teclado.nextInt());
+            opcion = teclado.nextInt();
+            
             System.out.println("Ingrese el primer numero: ");
-            operacion.setNumero1(teclado.nextInt());
+            a = teclado.nextInt();
             System.out.println("Ingrese el segundo numero: ");
-            operacion.setNumero2(teclado.nextInt());
-            operacion.calcularResultado();
-        } while (operacion.getOpcion() != 0);
+            b = teclado.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("La suma es: " + (a + b));
+                    break;
+                case 2:
+                    System.out.println("La resta es: " + (a - b));
+                    break;
+                case 3:
+                    System.out.println("La multiplicación es: " + (a * b));
+                    break;
+                case 4:
+                    if (b != 0) {
+                        System.out.println("La división es: " + (a / b));
+                    } else {
+                        System.out.println("Error: División por cero no permitida.");
+                    }
+                    break;
+            }
+        } while (opcion != 0);
     }
 }
